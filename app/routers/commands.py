e3b816bb82c5eebbf13a0f50d03c174293cf99ca
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2025-01-24 16:04:15 +0300

    add command

--- app/routers/commands.py
+++ app/routers/commands.py
@@ -11,10 +11,18 @@ router = Router()
 
 
 @router.message(Command("start"))
-@router.message(F.text.lower() == "start")
 async def cmd_start(message: types.Message) -> None:
     user_id = message.from_user.id
     user_name = message.from_user.username
     first_name = message.from_user.first_name
     chat_id = message.chat.id
-    await message.answer(f"привет {user_name}")
+    await message.answer(f"привет {user_name}\n тут будет описания всего что я умею")
+
+
+@router.message(Command("subscribe"))
+async def cmd_subscribe(message: types.Message) -> None:
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    first_name = message.from_user.first_name
+    chat_id = message.chat.id
+    await message.answer(f"привет {user_name}\n тут будет описания всего что я умею")
--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from aiogram import (
     Bot,
     Dispatcher,
 )
-from aiogram.enums.parse_mode import (
-    ParseMode,
-)
 from aiogram.fsm.storage.memory import (
     MemoryStorage,
 )
