2959993d30dbf5f86015d26dcf308ad86992eed8
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2025-01-27 17:45:32 +0300

    add read url

--- app/services/ozon.py
+++ app/services/ozon.py
@@ -1,3 +1,5 @@
+import re
+
 from aiogram import (
     types,
 )
@@ -7,6 +9,16 @@ class OzonSubsciberService:
 
     def __init__(self, message: types.Message) -> None:
         self.message = message
+        self.url = self.get_url()
+
+    def get_url(self) -> str:
+        if not self.message.text:
+            return ""
+
+        url_pattern = re.compile(r"https?://[^\s]+")
+        match = url_pattern.search(self.message.text)
+        return match.group(0) if match else ""
 
     def subscribe(self):
+        import pdb; pdb.set_trace();
         return "foo"
