997a5b0ef5936dbbd3a9817260e45d2e758d1d79
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2025-01-28 17:54:59 +0300

    refactor

--- main.py
+++ main.py
@@ -23,16 +23,10 @@ from app.routers import (
     ozon,
 )
 
-# from pyvirtualdisplay import (
-#     Display,
-# )
-
 
 dp = Dispatcher(storage=MemoryStorage())
-# display = Display(visible=0, size=(1920, 1080), backend="xvfb")
 
 async def main():
-    # display.start()
     bot = Bot(token=settings.bot_token)
     dp.include_router(commands.router)
     dp.include_router(ozon.router)
@@ -40,7 +34,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     log.info("application running successfully")
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-    # display.stop()
 
 
 if __name__ == "__main__":
