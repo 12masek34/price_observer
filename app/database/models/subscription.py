41cc03125b5f92f90f90cd7cfb4d07c04f3cbab6
Dmitriy Martys <martys.dmitriy@krasnoe-beloe.ru>
2025-01-29 16:54:18 +0300

    add cascade to product

--- app/database/models/subscription.py
+++ app/database/models/subscription.py
@@ -19,11 +19,15 @@ class Subscription(MyBase):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, index=True, nullable=False)
     chat_id = Column(Integer, index=True, nullable=False)
-    product_id = Column(Integer, ForeignKey("products.id"), nullable=True)
+    product_id = Column(Integer, ForeignKey("products.id", ondelete="CASCADE"), nullable=True)
     user_name = Column(String, nullable=True)
     url = Column(String, nullable=False)
     service_name = Column(String, nullable=False)
-    product = relationship("Product", back_populates="subscriptions", lazy="joined")
+    product = relationship(
+        "Product",
+        back_populates="subscriptions",
+        lazy="joined",
+    )
     price_history = relationship(
         "PriceHistory",
         back_populates="subscription",
